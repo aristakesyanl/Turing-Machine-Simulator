542820d5cb8e4429f05945e2ab32efec2d9b9fdb
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-04-14 16:41:53 +0400

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include"Turing.h"
+#include<fstream>
+#include<iostream>
+#include<sstream>
+#include<string>
+int main(int argc, char* argv[]) {
+    
+    for (int i = 1; i < argc; i++) {
+
+        std::string str(argv[i]);
+        DFA a(str);
+        std::string test;
+        std::cin>>test;
+        std::cout << a.run(test) << std::endl;;
+    }
+    
+}
