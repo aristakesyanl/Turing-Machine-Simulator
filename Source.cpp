bdf1159d5054f2147f1f3101ac65020f4e6d9b5c
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-04-15 14:16:53 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,28 @@
-#include"Turing.h"
-#include<fstream>
-#include<iostream>
-#include<sstream>
-#include<string>
 int main(int argc, char* argv[]) {
     
     for (int i = 1; i < argc; i++) {
 
         std::string str(argv[i]);
         Turing a(str);
-        std::string test;
-        std::cin>>test;
-        std::cout << a.run(test) << std::endl;
-        a.debug(test);
+        std::cout<<"If you want to run the program in mode type 'e' else type 'd'"<<std::endl;
+        char st;
+        std::cin>>st;
+        if(st=='e'){
+            std::cout<<"Enter Tape Input:"<<std::endl;
+            std::string test;
+            std::cin>>test;
+            std::cout << a.run(test) << std::endl;
+        }
+        else if(st=='d'){
+            std::cout<<"Enter Tape Input:"<<std::endl;
+            std::string test;
+            std::cin>>test;
+            a.debug(test);
+
+        }
+        else{
+            std::cerr<<"Invalid Instruction"<<std::endl;
+        }
     }
     
 }
